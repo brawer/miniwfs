71553be3bd921cc99c1f16de096176431c906ae5
Toni Suter <tonisuter@me.com>
2019-04-06 23:23:19 +0200

    listen for CREATE events in addition to the WRITE events

--- index.go
+++ index.go
@@ -210,7 +210,7 @@ func (index *Index) watchFiles() {
 			if !ok {
 				return
 			}
-			if event.Op&fsnotify.Write == fsnotify.Write {
+			if event.Op&fsnotify.Write == fsnotify.Write || event.Op&fsnotify.Create == fsnotify.Create {
 				path := event.Name
 				name := index.getCollectionNameForPath(path)
 				if name != "" {
