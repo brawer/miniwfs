d664d3fd3d04e1abd65373920bfb2f92283df1ec
Sascha Brawer <sascha@brawer.ch>
2019-04-11 13:59:24 +0200

    Move geometry operations into a separate file

---
+++ geometry.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"github.com/golang/geo/s2"
+	"github.com/paulmach/go.geojson"
+)
+
+func computeBounds(g *geojson.Geometry) s2.Rect {
+	r := s2.EmptyRect()
+	if g == nil {
+		return r
+	}
+
+	switch g.Type {
+	case geojson.GeometryPoint:
+		if len(g.Point) >= 2 {
+			r = r.AddPoint(s2.LatLngFromDegrees(g.Point[1], g.Point[0]))
+		}
+		return r
+
+	case geojson.GeometryMultiPoint:
+		for _, p := range g.MultiPoint {
+			if len(p) >= 2 {
+				r = r.AddPoint(s2.LatLngFromDegrees(p[1], p[0]))
+			}
+		}
+		return r
+
+	case geojson.GeometryLineString:
+		return computeLineBounds(g.LineString)
+
+	case geojson.GeometryMultiLineString:
+		for _, line := range g.MultiLineString {
+			r = r.Union(computeLineBounds(line))
+		}
+		return r
+
+	case geojson.GeometryPolygon:
+		for _, ring := range g.Polygon {
+			r = r.Union(computeLineBounds(ring))
+		}
+		s2.ExpandForSubregions(r)
+		return r
+
+	case geojson.GeometryMultiPolygon:
+		for _, poly := range g.MultiPolygon {
+			for _, ring := range poly {
+				r = r.Union(computeLineBounds(ring))
+			}
+			s2.ExpandForSubregions(r)
+		}
+		return r
+
+	case geojson.GeometryCollection:
+		for _, geometry := range g.Geometries {
+			r = r.Union(computeBounds(geometry))
+		}
+		return r
+
+	default:
+		return r
+	}
+}
+
+func computeLineBounds(line [][]float64) s2.Rect {
+	r := s2.EmptyRect()
+	for _, p := range line {
+		if len(p) >= 2 {
+			r = r.AddPoint(s2.LatLngFromDegrees(p[1], p[0]))
+		}
+	}
+	return r
+}
